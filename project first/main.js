88b84da7b2e149781d3e5dada77be4f2590367fd
vothaikiettt <kietthaivo2006@gmail.com>
2024-12-10 20:00:55 +0700

    Add files via upload

---
+++ project first/main.js
@@ -0,0 +1,23 @@
+{
+  /* <script>
+window.onload = setInterval(clock,1000);
+function clock()
+{
+var d = new Date();
+var date = d.getDate();
+var month = d.getMonth();
+var montharr =["Tháng 1","Tháng 2","tháng 3","Tháng 4","Tháng 5","Tháng 6","Tháng 7","Tháng 8","Tháng 9","Tháng 10","tháng 11","Tháng 12"];
+month=montharr[month];
+var year = d.getFullYear();
+var day = d.getDay();
+var dayarr =["Chủ Nhật","Thứ 2","Thứ 3","Thứ 4","Thứ 5","Thứ 6","Thứ 7"];
+day=dayarr[day];
+var hour =d.getHours();
+var min = d.getMinutes();
+var sec = d.getSeconds();
+document.getElementById("days").innerHTML=day;
+document.getElementById("dates").innerHTML=date+" "+month+" "+year;
+document.getElementById("times").innerHTML=hour+":"+min+":"+sec;
+}
+</script> */
+}
